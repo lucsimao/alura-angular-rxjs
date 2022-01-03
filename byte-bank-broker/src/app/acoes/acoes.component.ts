0f2b8b62e01bca463adc9d1003f88e333ed76439
lucsimao <lucasimao64@gmail.com>
2022-01-02 22:27:42 -0300

    feat: add unsubscribe method

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Acoes } from './modelos/acoes';
 import { AcoesService } from './acoes.services';
 import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit {
+export class AcoesComponent implements OnInit, OnDestroy {
   public acoesInput = new FormControl();
   public acoes: Acoes;
+  private subscription: Subscription;
 
   constructor(private acoesService: AcoesService) {}
 
   public ngOnInit(): void {
-    this.acoesService.getAcoes().subscribe((acoes) => {
+    this.subscription = this.acoesService.getAcoes().subscribe((acoes) => {
       this.acoes = acoes;
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
