4a58f240808ba85ac5915e5c12534aca2b93ef2e
lucsimao <lucasimao64@gmail.com>
2022-01-02 23:11:31 -0300

    refactor: use merge to allow start with all actions filtered

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -3,6 +3,7 @@ import { switchMap, tap } from 'rxjs/operators';
 import { AcoesService } from './acoes.services';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-acoes',
@@ -11,11 +12,15 @@ import { FormControl } from '@angular/forms';
 })
 export class AcoesComponent {
   public acoesInput = new FormControl();
-  public acoes$ = this.acoesInput.valueChanges.pipe(
-    tap(console.log),
-    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
-    tap(console.log)
+  public todasAcoes$ = this.acoesService
+    .getAcoes()
+    .pipe(tap(() => console.log('Fluxo Inicial')));
+  public filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    tap(() => console.log('Fluxo do Filtro')),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
   );
+
+  public acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
 
   constructor(private acoesService: AcoesService) {}
 }
